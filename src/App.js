fba250bb04e8773a85eed32053312830045a1e7a
hinkmond <git@lyriad.com>
2018-06-22 17:30:51 -0700

    Reformatted.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, Hello World #6! edit <code>src/App.js</code> and save to reload.
-        </p>
-          <p>
-              Test 20180622
-          </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, Hello World #6! edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    Test 20180622
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
